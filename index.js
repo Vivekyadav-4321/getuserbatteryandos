17c8ccde62800f5d58ba4bed03f2f5d97f8de231
vivek <smartvivekyadav9@gmail.com>
2022-02-11 16:43:43 +0530

    fixed the error

--- index.js
+++ index.js
@@ -1,42 +1,64 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const server = http.createServer(app);
+// const {Server, Socket} = require("socket.io")
+// const io = new Server(server);
+// const db  = require("mongoose")
+// const PORT = process.env.PORT ||5000;
+// db.connect("mongodb+srv://vievkyadav:guluthegreat@cluster0.tswwn.mongodb.net/users?retryWrites=true&w=majority")
+
+// const schema = db.Schema({
+//     username: String,
+//     battery: String,
+//     useros: String,
+//     TimeAndDate: String
+// })
+
+// const users = db.model("users",schema)
+
+
+// app.get("/", (req,res)=>{
+//     res.sendFile(`${__dirname}/index.html`)
+// })
+// app.get("/home", (req,res)=>{
+//     res.sendFile(`${__dirname}/index.html`)
+// })
+// io.on("connection",(socket)=>{
+// socket.on("userinfo",(userinfo)=>{
+// new users({battery: userinfo.userbattery, useros: userinfo.useros, username: userinfo.username, TimeAndDate: userinfo.datentime}).save()
+// })
+
+// })
+
+
+
+// server.listen(PORT,()=>{})
+
+
+const express = require("express")
+const app = express()
+const http = require("http")
+const server = http.createServer(app)
 const {Server, Socket} = require("socket.io")
-const io = new Server(server);
-const db  = require("mongoose")
+const io = new Server(server)
+
 const PORT = process.env.PORT ||5000;
-db.connect("mongodb+srv://vievkyadav:guluthegreat@cluster0.tswwn.mongodb.net/users?retryWrites=true&w=majority")
 
-const schema = db.Schema({
-    username: String,
-    battery: String,
-    useros: String,
-    TimeAndDate: String
+app.get("/",(req,res)=>{
+res.sendFile(`${__dirname}/index.html`,()=>{})
 })
 
-const users = db.model("users",schema)
+io.on("connection",(socket)=>{  
+    socket.setMaxListeners(8);
+    socket.on("clickedbutton",(r)=>{
+        socket.broadcast.emit("hehe",r)
+   
 
+}) 
+    })
 
-app.get("/", (req,res)=>{
-    res.sendFile(`${__dirname}/index.html`)
-})
-app.get("/home", (req,res)=>{
-    res.sendFile(`${__dirname}/index.html`)
-})
-io.on("connection",(socket)=>{
-socket.on("userinfo",(userinfo)=>{
-new users({battery: userinfo.userbattery, useros: userinfo.useros, username: userinfo.username, TimeAndDate: userinfo.datentime}).save()
-})
-
-})
-
-
-
-server.listen(PORT,()=>{})
-
-
-
+server.listen(PORT,()=>{console.log("server is live");})
 
 
 
